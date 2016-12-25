605a258a119edb583b891c6faa2b008c73f469c1
Yu Ushio <ush10yu@gmail.com>
2016-12-25 16:23:19 +0900

    Done Tutorial 1

---
+++ New Unity Project/Assets/BallController.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+var Speed = 20.0;
+		function Start () {
+			GetComponent.<Rigidbody>().AddForce(
+			( transform.forward + transform.right ) * Speed,
+			ForceMode.VelocityChange );
+		}
+function Update () {
+
+}
